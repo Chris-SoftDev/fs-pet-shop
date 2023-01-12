3a954e48d2c6bd5681e1e8dbd294fdd33a88ec1e
Christopher Ronca <christopherronca@gmail.com>
2023-01-11 18:20:50 -0700

    Project Part 1 - Node Filesystem Complete

--- pets.js
+++ pets.js
@@ -59,6 +59,25 @@ function updatePet(index, age, kind, name) {
     });
 }
 
+function destroyPet(index) {
+    let fileResult;
+    let deletedEntry; 
+    fs.readFile('./pets.json', 'utf8', function(error, data){
+        if(error){
+            console.log(error)
+        } else {
+            fileResult = JSON.parse(data);
+            deletedEntry = fileResult[index];
+            fileResult.splice(index, 1)
+            fs.writeFile('./pets.json', JSON.stringify(fileResult), function(error){
+                if (error) {
+                    console.log(error)
+                } else console.log(deletedEntry)
+            })
+        }
+    });
+}
+
 
 let cmdInput = process.argv;
 let cmdAction = cmdInput[2];
@@ -67,34 +86,50 @@ let cmdAge;
 let cmdKind;
 let cmdName;
 
-if (cmdAction === 'read') {
-    if (cmdIndex === undefined) {
-        readPets()  
-    } else specificPet(cmdIndex);
-} else if (cmdAction === 'create') {
+switch (cmdAction) {
+    case 'read':
+        cmdIndex = cmdInput[3];
+        if (cmdIndex === undefined) {
+            readPets()  
+        } else specificPet(cmdIndex);
+        break;
+
+    case 'create':
         cmdAge = cmdInput[3];
         cmdKind = cmdInput[4];
         cmdName = cmdInput [5];
-    if (cmdAge != undefined && cmdKind != undefined && cmdName != undefined) {
-        createPet(cmdAge, cmdKind, cmdName);
-    } else {
-        process.stderr.write('Usage: node pets.js create AGE KIND NAME');
-        process.exitCode = 1;
-    }
-} else if (cmdAction === 'update') {
-     cmdIndex = cmdInput[3];
-     cmdAge = cmdInput[4];
-     cmdKind = cmdInput[5];
-     cmdName = cmdInput [6];
-    if (cmdIndex != undefined && cmdAge != undefined && cmdKind != undefined && cmdName != undefined) {
-        updatePet(cmdIndex, cmdAge, cmdKind, cmdName);
-    } else {
-        process.stderr.write('Usage: node pets.js update INDEX AGE KIND NAME');
-        process.exitCode = 1;
-    }
-} else if (cmdAction === 'destroy') {
+        if (cmdAge != undefined && cmdKind != undefined && cmdName != undefined) {
+            createPet(cmdAge, cmdKind, cmdName);
+        } else {
+            process.stderr.write('Usage: node pets.js create AGE KIND NAME');
+            process.exitCode = 1;
+        }
+        break;
 
-} else {
-    process.stderr.write('Usage: node pets.js [read | create | update | destroy]');
-    process.exitCode = 1;
+    case 'update':
+        cmdIndex = cmdInput[3];
+        cmdAge = cmdInput[4];
+        cmdKind = cmdInput[5];
+        cmdName = cmdInput [6];
+        if (cmdIndex != undefined && cmdAge != undefined && cmdKind != undefined && cmdName != undefined) {
+            updatePet(cmdIndex, cmdAge, cmdKind, cmdName);
+        } else {
+            process.stderr.write('Usage: node pets.js update INDEX AGE KIND NAME');
+            process.exitCode = 1;
+        }
+        break;
+
+    case 'destroy':
+        cmdIndex = cmdInput[3]
+        if (cmdIndex != undefined) {
+            destroyPet(cmdIndex)
+        } else {
+            process.stderr.write('Usage: node pets.js destroy INDEX');
+            process.exitCode = 1;
+        }
+        break;
+
+    default:
+        process.stderr.write('Usage: node pets.js [read | create | update | destroy]');
+        process.exitCode = 1;
 }
